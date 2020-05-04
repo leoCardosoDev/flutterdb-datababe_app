52a749ebc6dca9c9033efde3d1930541aa4733e1
leoCardosoDev <leocardosodev@gmail.com>
2020-05-04 08:27:25 -0300

    Database com CRUD Completo

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,6 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
   _recuperarBancoDados() async {
     final caminhoBancoDados = await getDatabasesPath();
     final localBancoDados = join(caminhoBancoDados, "banco.db");
@@ -21,8 +20,9 @@ class _HomeState extends State<Home> {
     var db = await openDatabase(
       localBancoDados,
       version: 1,
-      onCreate: (db, dbVersaoRecente){
-        String sql = "CREATE TABLE usuarios (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR, idade INTEGER)";
+      onCreate: (db, dbVersaoRecente) {
+        String sql =
+            "CREATE TABLE usuarios (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR, idade INTEGER)";
         db.execute(sql);
       },
     );
@@ -31,26 +31,88 @@ class _HomeState extends State<Home> {
     return db;
   }
 
-
   _salvar() async {
     Database db = await _recuperarBancoDados();
 
     Map<String, dynamic> dadosUsuario = {
-      "nome" : "Priscila Cardoso",
-      "idade": 38
+      "nome": "Usuario 3 para Deletar",
+      "idade": 99
     };
 
-    int id = await db.insert('usuarios',  dadosUsuario);
+    int id = await db.insert('usuarios', dadosUsuario);
     print("Usuário CRIADO COM SUCESSO: ID -> " + id.toString());
   }
 
+  _listarUsuario() async {
+    Database db = await _recuperarBancoDados();
+    //String sql = "SELECT id, nome, idade FROM usuarios";
+    //String sql = "SELECT id, nome, idade FROM usuarios WHERE ID = 5";
+    String sql = "SELECT id, nome, idade FROM usuarios";
+    List usuarios = await db.rawQuery(sql);
+
+    //print("USUARIOS: " + usuarios.toString());
+
+    for (var usuario in usuarios) {
+      print(" ID: " +
+          usuario['id'].toString() +
+          "\n NOME: " +
+          usuario['nome'] +
+          "\n IDADE: " +
+          usuario['idade'].toString());
+    }
+  }
+
+  _listarUsuarioId(int id) async {
+    Database db = await _recuperarBancoDados();
+    List usuarios = await db.query("usuarios",
+        columns: ["id", "nome", "idade"], where: "id = ?", whereArgs: [id]);
+
+    for (var usuario in usuarios) {
+      print(" ID: " +
+          usuario['id'].toString() +
+          "\n NOME: " +
+          usuario['nome'] +
+          "\n IDADE: " +
+          usuario['idade'].toString());
+    }
+  }
+
+  _excluirUsuario(int id) async {
+    Database db = await _recuperarBancoDados();
+    int retorno = await db.delete("usuarios", where: "id = ?", whereArgs: [id]);
+
+    print("Quantidade de registro removido(s): $retorno" );
+  }
+
+  _atualizarUsuario(int id) async {
+    Database db = await _recuperarBancoDados();
+
+    Map<String, dynamic> dadosUsuario = {
+      "nome": "Jurema Cardoso de Oliveira",
+      "idade": 1
+    };
+
+    int retorno = await db.update(
+      "usuarios",
+      dadosUsuario,
+      where: "id = ?", whereArgs: [id]
+    );
+
+    print("Quantidade de usuario atualizado = $retorno" );
+  }
 
 
   @override
   Widget build(BuildContext context) {
-
     //_recuperarBancoDados();
-    _salvar();
+    //_salvar();
+    //_listarUsuario();
+    //_listarUsuarioId(6);
+    //_excluirUsuario(8);
+    //_atualizarUsuario(6);
+
+    _listarUsuario();
+
 
     return Container();
   }
